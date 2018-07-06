f1adca55af6fb80c286d13f189c2b95e6682db21
Zoran Ravic <contact@zoranravic.com>
2018-07-06 18:24:08 +0200

    Publishing docs

--- src/build.ts
+++ src/build.ts
@@ -14,7 +14,7 @@ async function start()
     const staticExport = container.get(IStaticExportService);
     console.log('Exporting Static Site...');
     const pages = await staticExport.getPages('/');
-    await staticExport.savePages('static', pages);
+    await staticExport.savePages('docs', pages);
     process.exit(0);
 }
 
